3014709a9a0925b4d3105fca7972fc9732a7637d
Your Name <you@example.com>
2025-01-19 22:04:38 -0400

    DC

--- api/routes/applicationsRoutes.js
+++ api/routes/applicationsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register,getApplications, getCustomers } from '../controllers/applicationsController.js';
+import { register,getApplications, getApplicationsToApp, getCustomers } from '../controllers/applicationsController.js';
 import multer from 'multer';
 
 const router = express.Router();
@@ -14,6 +14,7 @@ router.post('/register', upload.single('file'), register);
 // Rutas de autenticación
 // router.post('/register', register);
 router.get('/getApplications', getApplications);
+router.get('/getApplicationsToApp', getApplicationsToApp);
 router.get('/customers', getCustomers);
 
 export default router;
