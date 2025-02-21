2ad65c959b51982db0ee2594aaf308dca4150adf
David Conde <davidca926@gmail.com>
2025-02-21 17:23:49 -0400

    cambios david

--- api/controllers/auditController.js
+++ api/controllers/auditController.js
@@ -1,4 +1,4 @@
-import AuditLog from '../models/AuditCollection.js';
+import AuditLog from "../models/AuditCollection.js";
 
 // Crear un nuevo registro de auditoría
 export const createAuditLog = async (req, res) => {
@@ -7,7 +7,7 @@ export const createAuditLog = async (req, res) => {
     const savedLog = await newAuditLog.save();
     res.status(201).json(savedLog);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({message: error.message});
   }
 };
 
@@ -17,7 +17,7 @@ export const getAllAuditLogs = async (req, res) => {
     const logs = await AuditLog.find();
     res.json(logs);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({message: error.message});
   }
 };
 
@@ -25,21 +25,26 @@ export const getAllAuditLogs = async (req, res) => {
 export const getAuditLogById = async (req, res) => {
   try {
     const log = await AuditLog.findById(req.params.id);
-    if (!log) return res.status(404).json({ message: 'Registro no encontrado' });
+    if (!log) return res.status(404).json({message: "Registro no encontrado"});
     res.json(log);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({message: error.message});
   }
 };
 
 // Actualizar un registro de auditoría
 export const updateAuditLog = async (req, res) => {
   try {
-    const updatedLog = await AuditLog.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updatedLog) return res.status(404).json({ message: 'Registro no encontrado' });
+    const updatedLog = await AuditLog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true},
+    );
+    if (!updatedLog)
+      return res.status(404).json({message: "Registro no encontrado"});
     res.json(updatedLog);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({message: error.message});
   }
 };
 
@@ -47,9 +52,10 @@ export const updateAuditLog = async (req, res) => {
 export const deleteAuditLog = async (req, res) => {
   try {
     const deletedLog = await AuditLog.findByIdAndDelete(req.params.id);
-    if (!deletedLog) return res.status(404).json({ message: 'Registro no encontrado' });
-    res.json({ message: 'Registro eliminado exitosamente' });
+    if (!deletedLog)
+      return res.status(404).json({message: "Registro no encontrado"});
+    res.json({message: "Registro eliminado exitosamente"});
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({message: error.message});
   }
 };
