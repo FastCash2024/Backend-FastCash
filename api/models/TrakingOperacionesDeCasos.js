cff09e6c700cfa561f23c2c6a3ea3e311873e74f
Your Name <you@example.com>
2025-02-24 10:21:14 -0400

    DC

--- api/models/TrakingOperacionesDeCasos.js
+++ api/models/TrakingOperacionesDeCasos.js
@@ -1,26 +1,21 @@
 import mongoose from 'mongoose';
 
-// Esquema para Acotaciones del Auditor
-const AcotacionSchema = new mongoose.Schema({
-  fecha: String,
-  cuenta: String,
-  asesor: String,
-  emailAsesor: String,
-  acotacion: String,
-});
-
 // Esquema para el Tracking de Operaciones de Casos
 const TrackingDeOperacionesSchema = new mongoose.Schema({
   caso: String,
   seccion: String,
-  subseccion: String,
+  subID: String,
+  
+  codigoDeOperacion: String,
+  codigoDeProducto: String,
   operacion: String,
   modificacion: String,
   fecha: String,
   cuenta: String,
   asesor: String,
   emailAsesor: String,
-  acotacionesAuditor: [AcotacionSchema],
+  acotacion: String
 });
+
 
 export default mongoose.model('TrackingDeOperaciones', TrackingDeOperacionesSchema);
